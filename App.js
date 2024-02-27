d19496ff4dc12df888f33ae517544829951892b0
anilyavas <anil.yavas99@gmail.com>
2024-02-27 15:00:40 +0300

    list of flights with infos

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
 import SearchForm from './components/SearchForm';
+import FlightOptionItem from './components/FlightOptionItem';
+import { LinearGradient } from 'expo-linear-gradient';
+import data from './assets/data/data.json';
 
 const onSearch = async (data) => {
   console.log(data);
 };
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <SearchForm onSearch={onSearch} />
-      <StatusBar style='auto' />
-    </SafeAreaView>
+    <LinearGradient colors={['white', '#E0EFFF']} style={styles.container}>
+      <SafeAreaView>
+        <SearchForm onSearch={onSearch} />
+        <FlatList
+          data={data}
+          renderItem={(item) => <FlightOptionItem flight={item.item} />}
+        />
+        <StatusBar style='auto' />
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
